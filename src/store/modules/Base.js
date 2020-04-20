f5c97ce9c84298dcf7b08e260cf64c49d4489495
Everend <2322>
2020-04-20 16:40:50 +0800

    bug修改

--- src/store/modules/Base.js
+++ src/store/modules/Base.js
@@ -4,7 +4,7 @@ const Base = {
   state: {
     targetLevel: getUrlParam('targetLevel') || 2, //默认集团
     departmentID: getUrlParam('departmentID'),
-    departmentName:  getUrlParam('departmentID') || '移动管理驾驶舱'
+    departmentName:  getUrlParam('departmentName') || '移动管理驾驶舱'
   }
 };
 
